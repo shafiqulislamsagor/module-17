38a4a16bd398951165725eda6918b5d580892d92
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-27 02:03:21 +0600

    module 17.1

---
+++ 17.1.js
@@ -0,0 +1,14 @@
+/**
+ * Comparison
+ * 1. greater than : > 
+ * 2. less than: < 
+ * 3. equal : ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 
+ * 
+ * --------------------------
+ * 7. and: &&
+ * 8. or: ||
+ */
