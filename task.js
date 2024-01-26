bc17bc83824832e52e0db4b360cf4ef07b0072b0
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-27 04:31:02 +0600

    task 1 completed

---
+++ task.js
@@ -0,0 +1,11 @@
+const money = 200 ;
+
+if(money > 500){
+    let coke = 0 ;
+    console.log("I got free coke by buying food of "+ money + " tk and coke price: " + coke);
+}
+else{
+    let coke = 30 ;
+    console.log("I don't get free coke by buying food of "+ money + " tk and coke price: " + coke);
+}
+
