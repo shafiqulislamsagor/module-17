1f3fa43d4bc1b8e6f7a96f50d671a24f060ebe7c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-27 03:59:14 +0600

    semi advance ternary

--- 17.7.js
+++ 17.7.js
@@ -10,3 +10,20 @@
 
 // age >= 18 ? console.log("You can vote !!!") : console.log("You cann't vote !!!")
 
+let price = 10000 ;
+const isLeader = true;
+
+// if(isLeader === true) {
+//     price = 0;
+// }
+// else{
+//     price = price + 500;
+// }
+// console.log(price)
+
+// price = isLeader === true ? 0 : price + 200 ;
+// console.log(price)
+
+price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price;
+
+console.log(price);
