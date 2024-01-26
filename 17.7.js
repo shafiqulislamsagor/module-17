4b18c5e1f8b3184f83d56f9675b899cd9e33de0c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-27 03:56:12 +0600

    ternary simple

---
+++ 17.7.js
@@ -0,0 +1,12 @@
+/**
+ * Ternary operator
+ * 
+ * ternary three parts 
+ * 
+ * Condition ? do something when true : do something when false;
+ * 
+ */
+// const age = 18 ;
+
+// age >= 18 ? console.log("You can vote !!!") : console.log("You cann't vote !!!")
+
