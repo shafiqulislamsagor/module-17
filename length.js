402ea18bf14cbc67eaa78c4706af8525469b3ed5
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-27 19:18:15 +0600

    length property

---
+++ length.js
@@ -0,0 +1,11 @@
+const num = [12 , 24, 344 , 33 ,  34, 35];
+console.log(num.length);
+
+console.log(num[0]);
+console.log(num[1]);
+console.log(num[3]);
+console.log(num);
+
+const forth = num[5];
+
+console.log(forth)
