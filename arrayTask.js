018d0c9f8f5e1dced985041bcc3d378d5a6e558d
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-29 01:50:24 +0600

    array task 2

--- arrayTask.js
+++ arrayTask.js
@@ -5,3 +5,14 @@ console.log(fruits[3]);
 
 fruits[2] = 'Jambura';
 console.log(fruits);
+
+// Task 2
+const tourist = ['Modutila' ,'Gojni' ,'Safari'];
+
+tourist.push('GreenPark');
+
+tourist.push('goldenPark', 'BluePark');
+
+tourist.pop();
+
+console.log(tourist)
